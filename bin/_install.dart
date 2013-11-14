34a695155c57516615bd4bca854db04ba8ca6e01
andrew <andrew.mezoni@gmail.com>
2013-11-14 20:34:26 +0600

    0.0.2

--- bin/_install.dart
+++
@@ -1,5 +0,0 @@
-// Not required to execute this script manually
-void main(List<String> args) {
-  print("'bin/_install.dart': I was launched automatically during installation process.");
-  print("All similar files in all dependent packages will be executed automatically during installation.");
-}
